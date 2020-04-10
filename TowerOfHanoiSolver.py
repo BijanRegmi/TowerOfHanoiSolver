925a939b976ccce82b9f4dc61c8aa70f2ab513c6
BijanRegmi <vizandabijan@gmail.com>
2020-04-10 16:34:30 +0545

    Initia Commit

---
+++ TowerOfHanoiSolver.py
@@ -0,0 +1,17 @@
+def Solver(n,L,M,R):
+    if n:
+        Solver(n-1,L,R,M)
+        print("%s to %s"%(L,R))
+        Solver(n-1,M,L,R)
+
+def main():
+    num = int(input("ENTER NUMBER OF DISK "))
+    while num<3:
+        num = int(input("ENTER NUMBER GREATER THAN 2 "))
+    source = "L"
+    destination = "R"
+    medium = "M"
+    Solver(num,source,medium,destination)
+
+if __name__ == "__main__":
+    main()
